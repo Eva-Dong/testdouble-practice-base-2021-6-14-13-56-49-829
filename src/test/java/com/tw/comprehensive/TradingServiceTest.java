2e34ad35e25b1286c5c1aae27ecab518f765aeab
Eva <eva.dong@cargosmart.com>
2021-06-16 19:57:40 +0800

    finish task 1

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,24 @@
 package com.tw.comprehensive;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class TradingServiceTest {
+
+    @Test
+    public void should_call_log_new_trade_method_when_execute_create_trade(){
+        //given
+        AuditService auditService = mock(AuditService.class);
+        TradeRepository tradeRepository = new TradeRepository();
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        Trade trade = new Trade("1" ,"2");
+
+        //when
+        tradingService.createTrade(trade);
+
+        //then
+        verify(auditService).logNewTrade(trade);
+    }
 
 }
