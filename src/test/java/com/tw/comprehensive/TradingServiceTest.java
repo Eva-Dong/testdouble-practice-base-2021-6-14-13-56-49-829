9bdce1c7964d166c5ba51954713163e9f0c41749
Eva <eva.dong@cargosmart.com>
2021-06-16 19:58:26 +0800

    finish task 2

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,8 +1,11 @@
 package com.tw.comprehensive;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 class TradingServiceTest {
 
@@ -19,6 +22,22 @@ class TradingServiceTest {
 
         //then
         verify(auditService).logNewTrade(trade);
+    }
+
+    @Test
+    public void should_return_same_value_between_find_trade_and_find_by_id(){
+        //given
+        AuditService auditService = mock(AuditService.class);
+        TradeRepository tradeRepository = mock(TradeRepository.class);
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        Trade expectedTrade = new Trade("1" ,"2");
+        doReturn(expectedTrade).when(tradeRepository).findById(any());
+
+        //when
+        Trade trade = tradingService.findTrade(anyLong());
+
+        //then
+        assertEquals(trade, expectedTrade);
     }
 
 }
